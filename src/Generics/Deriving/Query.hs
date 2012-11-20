137e555a8144e09487826cfc1b791ea422cc2551
Sean Leather <leather@cs.uu.nl>
2012-11-20 22:43:06 +0100

    Reduce QueryWith and Collect to just Query

--- src/Generics/Deriving/Query.hs
+++ src/Generics/Deriving/Query.hs
@@ -6,99 +6,66 @@
 {-# LANGUAGE OverlappingInstances #-}
 
 module Generics.Deriving.Query (
-  QueryWith(..),
-  queryWithdefault,
-  Collect(..),
-  collectdefault,
-  collectempty,
+  Q,
+  Query(..),
+  mquery,
+  collect,
+  querydefault,
+  queryapply,
+  queryempty,
 ) where
 
 --------------------------------------------------------------------------------
 
 import GHC.Generics
-import Data.Monoid
-import Control.Applicative
+import Data.Monoid (Monoid(mempty, mappend))
+import Control.Applicative (pure, Alternative(empty, (<|>)))
 
 --------------------------------------------------------------------------------
 
--- | Type-indexed function for monoidal querying
-class QueryWith' f a where
-  queryWith' :: Monoid b => (a -> b) -> f c -> b
+type Q c a b = b -> (b -> b -> b) -> (a -> b) -> c -> b
 
-instance QueryWith' U1 a where
-  queryWith' _ U1 = mempty
+-- | The non-exported type-indexed function
 
-instance QueryWith' (K1 i a) a where
-  queryWith' f (K1 x) = f x
+class Query' f a where
+  query' :: Q (f x) a b
 
-instance QueryWith b a => QueryWith' (K1 i b) a where
-  queryWith' f (K1 x) = queryWith f x
+instance Query' U1 a where
+  query' e _ _ U1 = e
 
-instance QueryWith' f a => QueryWith' (M1 i c f) a where
-  queryWith' f (M1 x) = queryWith' f x
+instance Query b a => Query' (K1 i b) a where
+  query' e p f (K1 x) = query e p f x
 
-instance (QueryWith' f a, QueryWith' h a) => QueryWith' (f :+: h) a where
-  queryWith' f (L1 x) = queryWith' f x
-  queryWith' f (R1 x) = queryWith' f x
+instance Query' f a => Query' (M1 i c f) a where
+  query' e p f (M1 x) = query' e p f x
 
-instance (QueryWith' f a, QueryWith' h a) => QueryWith' (f :*: h) a where
-  queryWith' f (x :*: y) = queryWith' f x `mappend` queryWith' f y
+instance (Query' f a, Query' h a) => Query' (f :+: h) a where
+  query' e p f (L1 x) = query' e p f x
+  query' e p f (R1 x) = query' e p f x
 
---------------------------------------------------------------------------------
-
-queryWithdefault :: (Generic c, QueryWith' (Rep c) a, Monoid b) => (a -> b) -> c -> b
-queryWithdefault f = queryWith' f . from
+instance (Query' f a, Query' h a) => Query' (f :*: h) a where
+  query' e p f (x :*: y) = query' e p f x `p` query' e p f y
 
 --------------------------------------------------------------------------------
 
-class QueryWith c a where
-  queryWith :: Monoid b => (a -> b) -> c -> b
+querydefault :: (Generic c, Query' (Rep c) a) => Q c a b
+querydefault e p f = query' e p f . from
 
-  default queryWith :: (Generic c, QueryWith' (Rep c) a, Monoid b) => (a -> b) -> c -> b
-  queryWith = queryWithdefault
+queryapply :: Q a a b
+queryapply _ _ f x = f x
 
-instance QueryWith Char   a where queryWith _ = mempty
-instance QueryWith Int    a where queryWith _ = mempty
-instance QueryWith Float  a where queryWith _ = mempty
-instance QueryWith Bool   a where queryWith _ = mempty
+queryempty :: Q c a b
+queryempty e _ _ _ = e
 
 --------------------------------------------------------------------------------
 
--- | Type-indexed function for alternative collecting
-class Collect' f a where
-  collect' :: Alternative g => f c -> g a
+class Query c a where
+  query :: Q c a b
 
-instance Collect' U1 a where
-  collect' U1 = empty
-
-instance Collect b a => Collect' (K1 i b) a where
-  collect' (K1 x) = collect x
-
-instance Collect' f a => Collect' (M1 i c f) a where
-  collect' (M1 x) = collect' x
-
-instance (Collect' f a, Collect' h a) => Collect' (f :+: h) a where
-  collect' (L1 x) = collect' x
-  collect' (R1 x) = collect' x
-
-instance (Collect' f a, Collect' h a) => Collect' (f :*: h) a where
-  collect' (x :*: y) = collect' x <|> collect' y
+  default query :: (Generic c, Query' (Rep c) a) => Q c a b
+  query = querydefault
 
 --------------------------------------------------------------------------------
-
-collectdefault :: (Generic c, Collect' (Rep c) a, Alternative f) => c -> f a
-collectdefault = collect' . from
-
-collectempty :: Alternative f => b -> f a
-collectempty _ = empty
-
---------------------------------------------------------------------------------
-
-class Collect b a where
-  collect :: Alternative f => b -> f a
-
-  default collect :: (Generic b, Collect' (Rep b) a, Alternative f) => b -> f a
-  collect = collectdefault
 
 -- Each type requires two instances.
 
@@ -110,47 +77,57 @@ class Collect b a where
 -- instances need to stop the recursion. Since no other type is found in them
 -- type, we use 'collectempty' for the default instance.
 
-instance Collect Char Char where collect = pure
-instance Collect Char b    where collect = collectempty
+instance Query Char Char where query = queryapply
+instance Query Char b    where query = queryempty
 
-instance Collect Int Int where collect = pure
-instance Collect Int b   where collect = collectempty
+instance Query Int Int where query = queryapply
+instance Query Int b   where query = queryempty
 
-instance Collect Float Float where collect = pure
-instance Collect Float b     where collect = collectempty
+instance Query Float Float where query = queryapply
+instance Query Float b     where query = queryempty
 
-instance Collect Double Double where collect = pure
-instance Collect Double b      where collect = collectempty
+instance Query Double Double where query = queryapply
+instance Query Double b      where query = queryempty
 
-instance Collect Bool Bool where collect = pure
-instance Collect Bool b    where collect = collectempty
+instance Query Bool Bool where query = queryapply
+instance Query Bool b    where query = queryempty
 
-instance Collect () () where collect = pure
-instance Collect () b  where collect = collectempty
+instance Query () () where query = queryapply
+instance Query () b  where query = queryempty
 
 -- For all other types, the default instance is standard.
 
-instance Collect (Maybe b) (Maybe b) where collect = pure
-instance Collect b a => Collect (Maybe b) a
+instance Query (Maybe b) (Maybe b) where query = queryapply
+instance Query b a => Query (Maybe b) a
 
-instance Collect [b] [b] where collect = pure
-instance Collect b a => Collect [b] a
+instance Query [b] [b] where query = queryapply
+instance Query b a => Query [b] a
 
-instance Collect (b,c) (b,c) where collect = pure
-instance (Collect b a, Collect c a) => Collect (b,c) a
+instance Query (b,c) (b,c) where query = queryapply
+instance (Query b a, Query c a) => Query (b,c) a
 
-instance Collect (b,c,d) (b,c,d) where collect = pure
-instance (Collect b a, Collect c a, Collect d a) => Collect (b,c,d) a
+instance Query (b,c,d) (b,c,d) where query = queryapply
+instance (Query b a, Query c a, Query d a) => Query (b,c,d) a
 
-instance Collect (b,c,d,e) (b,c,d,e) where collect = pure
-instance (Collect b a, Collect c a, Collect d a, Collect e a) => Collect (b,c,d,e) a
+instance Query (b,c,d,e) (b,c,d,e) where query = queryapply
+instance (Query b a, Query c a, Query d a, Query e a) => Query (b,c,d,e) a
 
-instance Collect (b,c,d,e,f) (b,c,d,e,f) where collect = pure
-instance (Collect b a, Collect c a, Collect d a, Collect e a, Collect f a) => Collect (b,c,d,e,f) a
+instance Query (b,c,d,e,f) (b,c,d,e,f) where query = queryapply
+instance (Query b a, Query c a, Query d a, Query e a, Query f a) => Query (b,c,d,e,f) a
 
-instance Collect (b,c,d,e,f,g) (b,c,d,e,f,g) where collect = pure
-instance (Collect b a, Collect c a, Collect d a, Collect e a, Collect f a, Collect g a) => Collect (b,c,d,e,f,g) a
+instance Query (b,c,d,e,f,g) (b,c,d,e,f,g) where query = queryapply
+instance (Query b a, Query c a, Query d a, Query e a, Query f a, Query g a) => Query (b,c,d,e,f,g) a
 
-instance Collect (b,c,d,e,f,g,h) (b,c,d,e,f,g,h) where collect = pure
-instance (Collect b a, Collect c a, Collect d a, Collect e a, Collect f a, Collect g a, Collect h a) => Collect (b,c,d,e,f,g,h) a
+instance Query (b,c,d,e,f,g,h) (b,c,d,e,f,g,h) where query = queryapply
+instance (Query b a, Query c a, Query d a, Query e a, Query f a, Query g a, Query h a) => Query (b,c,d,e,f,g,h) a
+
+--------------------------------------------------------------------------------
+
+mquery :: (Query c a, Monoid b) => (a -> b) -> c -> b
+mquery = query mempty mappend
+
+collect :: (Query c a, Alternative f) => c -> f a
+collect = query empty (<|>) pure
+
+--------------------------------------------------------------------------------
 
