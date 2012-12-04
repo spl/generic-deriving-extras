ccaa98716dba4708c4453ac22238723bf3ce985d
Sean Leather <leather@cs.uu.nl>
2012-12-04 12:14:23 +0100

    Use Context module for Ctx

--- src/Generics/Deriving/Zipper/Base.hs
+++ src/Generics/Deriving/Zipper/Base.hs
@@ -31,28 +31,14 @@ module Generics.Deriving.Zipper.Base (
 
 --------------------------------------------------------------------------------
 
+import GHC.Generics
+import Generics.Deriving.Zipper.Context
+
 import Data.Typeable (Typeable, cast)
-import Generics.Deriving.Base
 
 import Control.Arrow (second)
 import Control.Applicative ((<$>), (<*>), (<|>))
 import Data.Maybe (fromJust)
-
---------------------------------------------------------------------------------
-
-data family Ctx (f :: * -> *) p :: *
-
-data instance Ctx U1 p
-
-data instance Ctx (K1 i a) p = CK
-
-newtype instance Ctx (M1 i c f) p = CM (Ctx f p)
-
-data instance Ctx (f :+: g) p = CL (Ctx f p)
-                              | CR (Ctx g p)
-
-data instance Ctx (f :*: g) p = C1 (g p) (Ctx f p)
-                              | C2 (f p) (Ctx g p)
 
 --------------------------------------------------------------------------------
 
